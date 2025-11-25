ad899617b4c2334583a7e5d8822214b336b75c3a
JoaoPedro265 <161182505+JoaoPedro265@users.noreply.github.com>
2025-11-25 18:48:58 +0000

    makes `database.js` `client.connect()` more robust to errors

--- infra/database.js
+++ infra/database.js
@@ -8,12 +8,20 @@ async function query(queryObject) {
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
   }); //cria um objeto pra se conectar ao db
-  await client.connect(); //conectar no db
+  console.log("credencias db", {
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+  });
   try {
+    await client.connect(); //conectar no db
     const result = await client.query(queryObject); //recebe qualquer query do codigo
     return result; //É o objeto que o PostgreSQL devolve depois que você executa uma query.
   } catch (error) {
     console.log(error);
+    throw error;
   } finally {
     await client.end();
   }
